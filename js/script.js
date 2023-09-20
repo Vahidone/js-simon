d728c070921708b7aa77830e0fb3a8f579ff5c0c
Vahidone <v.ban.roma@gmail.com>
2023-09-20 17:58:53 +0200

    feat: add function for counting guessed numbers

--- js/script.js
+++ js/script.js
@@ -16,16 +16,23 @@ for (i = 0; i < 5; i++) {
 
 setTimeout(function () {
 
-  casualNumbers.innerHTML = '';
+  casualNumbers.textContent = '';
 
   for (i = 0; i < 5; i++) {
     const numberUser = parseInt(prompt('inserisci un numero' + (i + 1)));
     guessNumbers.push(numberUser);
-    
-    
-
   }
 
+  let correctNumber = 0;
+  for (i = 0; i < casualNumbersArray.length; i++) {
+    const numeroCasuale = casualNumbersArray[i];
+    
+    if (guessNumbers.includes(numeroCasuale)) correctNumber++;
+  }
+  
+  let risultato = 'hai indovinato' + correctNumber + 'numeri';
+
+ 
 
 }, 5000);
 
